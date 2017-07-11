3c2e2ded5316f77f6bdbd28b4d3d45d873230deb
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 17:39:53 -0400

    Add Power class

---
+++ COMP123-S2017-Lesson9B/Power.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version: 0.1 - Created the Power class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+
+    /// <summary>
+    /// This is the Power class which will be used as a Data type
+    /// </summary>
+    public class Power
+    {
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        // CONSTRUCTORS -----------------------------------
+    }
+}
