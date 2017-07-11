b3463d046f8b8e24458b07bc9965179f447f32ef
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 17:35:46 -0400

    Added SuperHuman Class

---
+++ COMP123-S2017-Lesson9B/SuperHuman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman class that extends the Human abstract class
+ * Version: 0.1 - Created the SuperHuman Class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the SuperHuman class.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
