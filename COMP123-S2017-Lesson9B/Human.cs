c5bb6aaa0869e244139f799f4b0f2c9f8f27d66b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 17:29:20 -0400

    Added Human Abstract class

---
+++ COMP123-S2017-Lesson9B/Human.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 11, 2017
+ * Description: This is the Abstract Human class
+ * Version: 0.1 - Created the Abstract Human Class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the Abstract Human Class
+    /// </summary>
+    public abstract class Human
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private string _name;
+
+        // PUBLIC PROPERTIES
+        public string Name {
+
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        // CONSTRUCTORS ---------------------------------------------
+
+        /// <summary>
+        /// This is the main constructor for the Human class.
+        /// It takes one parameter - name (string)
+        /// </summary>
+        /// <param name="name"></param>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+
+    }
+}
