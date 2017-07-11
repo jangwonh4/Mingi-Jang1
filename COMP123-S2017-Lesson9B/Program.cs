a1ec6887723c392df35b2192c454c5674b47a15b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 17:12:23 -0400

    Modified Program.cs

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version: 0.1 - Created the Program class
+ */
+
 namespace COMP123_S2017_Lesson9B
 {
     class Program
