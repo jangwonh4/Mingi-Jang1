0d519d1ee1c3c41bec9ff78e221827614e521ab7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-11 18:19:26 -0400

    Displayed all powers in the Powers List - Driver class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: Demo for Lesson 9
- * Version: 0.3 - Added a Power to the SuperHuman object
+ * Version: 0.4 - Added another Power to the SuperHuman object
+ *              - Also displayed all the powers in the Powers List
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -19,6 +20,8 @@ namespace COMP123_S2017_Lesson9B
         {
             SuperHuman superHuman = new SuperHuman("Super Dude");
             superHuman.AddPower("Spider Climbing", 50);
+            superHuman.AddPower("Webcasting", 40);
+            superHuman.DisplayPowers();
         }
     }
 }
