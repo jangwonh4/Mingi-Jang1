19cee60c22e9c0b6de3867371f16a75e70f91e84
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 11:58:01 -0400

    Modified Driver class to implement SuperHero

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: Demo for Lesson 9
- * Version: 0.5 - implemented the ToString method of the superhuman class
+ * Version: 0.7 - Modified Driver class to implement SuperHero
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -17,10 +17,12 @@ namespace COMP123_S2017_Lesson9B
     {
         static void Main(string[] args)
         {
-            SuperHuman superHuman = new SuperHuman("Super Dude");
-            superHuman.AddPower("Spider Climbing", 50);
-            superHuman.AddPower("Webcasting", 40);
-            Console.WriteLine(superHuman.ToString());
+            SuperHero superHero = new SuperHero("Super Dude", 20);
+            superHero.AddPower("Spider Climbing", 50);
+            superHero.AddPower("Webcasting", 40);
+            Console.WriteLine(superHero.ToString());
+
+
         }
     }
 }
