2c379566d6a1619c6b78b5f6fb9a3407745dcaee
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 11:35:54 -0400

    Added IHasKarma interface

---
+++ COMP123-S2017-Lesson9B/IHasKarma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 13, 2017
+ * Description: This Interface lists the signature for any Super Hero Subclass
+ * Version: 0.1 - declared the Karma property
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+
+    public interface IHasKarma
+    {
+        // PUBLIC PROPERTIES
+        int Karma { get; set; } 
+    }
+}
