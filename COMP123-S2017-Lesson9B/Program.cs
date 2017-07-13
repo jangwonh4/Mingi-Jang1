9d8a51fa02fc142e5959336f429a3bbcffa13a45
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 11:03:43 -0400

    implemented the ToString method of the SuperHuman class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,8 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: Demo for Lesson 9
- * Version: 0.4 - Added another Power to the SuperHuman object
- *              - Also displayed all the powers in the Powers List
+ * Version: 0.5 - implemented the ToString method of the superhuman class
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -21,7 +20,7 @@ namespace COMP123_S2017_Lesson9B
             SuperHuman superHuman = new SuperHuman("Super Dude");
             superHuman.AddPower("Spider Climbing", 50);
             superHuman.AddPower("Webcasting", 40);
-            superHuman.DisplayPowers();
+            Console.WriteLine(superHuman.ToString());
         }
     }
 }
