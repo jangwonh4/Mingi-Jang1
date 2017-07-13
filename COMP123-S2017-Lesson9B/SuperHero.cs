9ac19ccc4218fd0110ac89b531bb1ba6e5e831aa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-13 11:47:50 -0400

    Added SuperHero Child class

---
+++ COMP123-S2017-Lesson9B/SuperHero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 13, 2017
+ * Description: This is the SuperHero class which inherits from the SuperHuman class
+ * Version: 0.1 - implemented the ToString method of the superhuman class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the SuperHero class
+    /// </summary>
+    public class SuperHero : SuperHuman, IHasKarma
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private int _karma;
+
+        // PUBLIC PROPERTIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the SuperHero class
+        /// It takes two arguments - name (string) - karma (int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="karma"></param>
+        public SuperHero(string name, int karma)
+            :base(name)
+        {
+            this.Karma = karma;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
